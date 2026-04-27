96622f7b4549dc614069ed749304c1006532638e
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-04-27 12:59:39 +0600

    uthentication urls

---
+++ metamec/authentication/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from .views import (
+    SignupView,
+    VerifyEmailView,
+    ResendEmailOTPView,
+    LoginView,
+    ForgotPasswordView,
+    VerifyPasswordResetOTPView,
+    ResetPasswordView,
+    MeView,
+)
+
+urlpatterns = [
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("verify-email/", VerifyEmailView.as_view(), name="verify_email"),
+    path("resend-email-otp/", ResendEmailOTPView.as_view(), name="resend_email_otp"),
+
+    path("login/", LoginView.as_view(), name="login"),
+    path("me/", MeView.as_view(), name="me"),
+
+    path("forgot-password/", ForgotPasswordView.as_view(), name="forgot_password"),
+    path("verify-reset-otp/", VerifyPasswordResetOTPView.as_view(), name="verify_reset_otp"),
+    path("reset-password/", ResetPasswordView.as_view(), name="reset_password"),
+]
