f4275157ba11843625ac4bce1e4cda3726a6e369
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2026-04-28 10:34:50 +0600

    otp showing in terminal

--- metamec/metamec/settings.py
+++ metamec/metamec/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
